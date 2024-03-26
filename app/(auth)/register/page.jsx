779e8cb5151f7b979a898c686900c2640fdded37
RajrupDasid <rajrupofficialid@protonmail.com>
2024-03-26 21:50:11 +0530

    user registration done
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,5 +1,45 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { register } from "@/lib/register";
+import GlobalApi from "@/utils/GlobalApi";
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [state, formAction] = useFormState(register, undefined);
+
+  console.log(state);
+  // const router = useRouter();
+  // // useEffect(() => {
+  // //   state?.success && router.push("/signin");
+  // // }, [state?.success, router]);
+  return (
+    <div className="flex items-baseline justify-center my-10">
+      <div className="flex flex-col items-center justify-center p-10 bg-slate-100 border border-slate-200">
+        <Image src="/logo.png" width={100} height={100} alt="registerlogo" />
+        <h2 className="font-bold text-3xl">Create account</h2>
+        <h2>Enter your email and password to create your account</h2>
+        <div>
+          <form action={formAction} className="flex flex-col w-full mt-7 gap-7">
+            <Input placeholder="username" name="username" type="text" />
+            <Input placeholder="name@example.com" name="email" type="email" />
+            <Input placeholder="password" name="password" type="password" />
+            <Button>Create an Account</Button>
+            {/* <span>{state?.error}</span> */}
+            <p>
+              Already have an account
+              <Link href={"/signin"} className="text-blue-500">
+                click here to signin
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
--- utils/GlobalApi.js
+++ utils/GlobalApi.js
@@ -22,6 +22,17 @@ const getProductsByCategory = (category) =>
   axiosClient
     .get("/products?filters[categories][name][$in]=" + category + "&populate=*")
     .then((resp) => resp.data.data);
+const registerUser = (username, email, password) =>
+  axiosClient
+    .post("/auth/local/register", {
+      username: username,
+      email: email,
+      password: password,
+    })
+    .then((resp) => {
+      resp.data.user;
+      resp.data.jwt;
+    });
 
 export default {
   getCategory,
@@ -30,4 +41,5 @@ export default {
   getAllProducts,
   getFooterBanner,
   getProductsByCategory,
+  registerUser,
 };
